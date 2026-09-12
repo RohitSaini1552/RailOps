d08e38d0d238f2905934f77a1f8098ba6112f688
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-09-12 21:33:26 +0530

    feat : add prometheus endpoints for scraping backend metrics

--- backend/src/sockets/seatSocket.js
+++ backend/src/sockets/seatSocket.js
@@ -1,13 +1,21 @@
 const { getTrainRoomName } = require('../services/seatLockService');
+const { socketConnections, socketRoomJoins } = require('../metrics');
 
 function initSeatSocket(io) {
   io.on('connection', (socket) => {
+    socketConnections.inc();
+
+    socket.on('disconnect', () => {
+      socketConnections.dec();
+    });
+
     socket.on('join-seat-room', ({ trainId, journeyDate }) => {
       if (!trainId || !journeyDate) {
         return;
       }
 
       socket.join(getTrainRoomName(trainId, journeyDate));
+      socketRoomJoins.inc();
     });
 
     socket.on('leave-seat-room', ({ trainId, journeyDate }) => {
